d6dc8a204fcf6c40953c2572c90816b62be75a5d
Alex Duncan <monkeymedic26@gmail.com>
2021-03-22 12:19:47 -0700

    adjusted frontend to pull requests from my service

--- server/index.js
+++ server/index.js
@@ -58,9 +58,7 @@ app.get('/api/reviews/meta/:product_id', (req, res) => {
 
 app.get('/questions/:params', (req, res) => {
   const { params } = req.params;
-  axios.get(`${url}qa/questions/?product_${params}`, {
-    headers: { Authorization: TOKEN },
-  })
+  axios.get(`http://localhost:3001/questions/${params}`)
     .then((data) => res.send(data.data))
     .catch((err) => console.log('error getting questions', err.response.data));
 });
